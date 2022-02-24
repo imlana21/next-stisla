80c62ed7f666db321e35d50c847bfb6b8c88989a
lana404 <ihwan.cfc@gmail.com>
2022-02-24 22:56:28 +0700

    add:jquery-package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,31 @@ import Head from 'next/head';
 import '@styles/globals.scss';
 import '@styles/stisla/components.css';
 import '@styles/stisla/style.css';
-import '../public/modules/bootstrap/css/bootstrap.min.css';
 import '../public/modules/fontawesome/css/all.min.css';
 import Script from 'next/script';
 import { useEffect } from 'react';
 
+// Declare window to local variable
+declare const window: any;
+
+// CSS 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Stisla Package
 import moment from 'moment';
+import * as jQuery from 'jquery';
+
+// Require Package
+require('jquery-ui-dist/jquery-ui');
+require('jquery.nicescroll/jquery.nicescroll');
+require('jquery-sparkline/jquery.sparkline');
+
 
 function MyApp({ Component, pageProps }: any): JSX.Element {
   useEffect( () => {
-    window.moment = require('/modules/moment.min.js');
+    window.moment = moment;
+    window.jQuery = jQuery;
+    window.$ = jQuery
   });
 
   return (
@@ -23,16 +38,13 @@ function MyApp({ Component, pageProps }: any): JSX.Element {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-      <Script src="/modules/jquery.min.js" onLoad={ () => console.log('JQuery Loaded') }/>
       <Script src="/modules/popper.js" onLoad={ () => console.log('Popper Loaded') }/>
       <Script src="/modules/tooltip.js" onLoad={ () => console.log('Tooltip Loaded') }/>
-      <Script src="/modules/nicescroll/jquery.nicescroll.min.js" onLoad={ () => console.log('NiceScroll Loaded') }/>
-      <Script src="" onLoad={ () => console.log('Moment Loaded') }/>
       <Script src="/modules/bootstrap/js/bootstrap.min.js" onLoad={ () => console.log('Popper Loaded') }/>
       <Script src="/js/stisla.js"/>
       <Script src="/js/scripts.js"/>
     </div>
-  )
+  );
 }
 
 export default MyApp
